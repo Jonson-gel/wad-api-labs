314cad44d855c9bc54985d0fc628bf78e95263f8
Jonson-gel <2904180191@qq.com>
2024-12-20 23:51:40 +0800

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -3,8 +3,20 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  username: { type: String, unique: true, required: true },
+  password: { 
+    type: String, 
+    required: true, 
+    validate: {
+      validator: passwordValidator,
+      message: 'Password must contain at least 8 characters, including one letter, one number, and one special character.'
+    }
+  }
 });
+
+const passwordValidator = (password) => {
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return passwordRegex.test(password);
+};
 
 export default mongoose.model('User', UserSchema);
